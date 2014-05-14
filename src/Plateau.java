a73d579e16b20c53551d82071222d18a7f5274a9
evagabriela <gabriela.osu@gmail.com>
2014-05-13 17:46:49 -0700

    Add override equals and hash code method for testing purposes

--- src/Plateau.java
+++ src/Plateau.java
@@ -6,19 +6,32 @@ public class Plateau {
     private final int height;
     private final int width;
 
-//     the first line on the file is the size of the grid
-
     public Plateau(int height, int width){
         this.height = height;
         this.width = width;
     }
 
-    public void add(Rover rover1) {
+    public int getPlateauHeight() {
+        return height;
+    }
 
+    @Override
+    public boolean equals(Object object){
+        if (this == object && object != null){
+            return true;
+        } else if (this.height == ((Plateau) object).height && this.width == ((Plateau) object).width ){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int result = height;
+        result = 31 * result + width;
+        return result;
     }
 
 
-    public String roverInPlateau() {
-        return null;
-    }
 }
