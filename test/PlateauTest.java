9afcd7bd8c7c9e3c8b18d7d81cd2a3ceadafd6f5
evagabriela <gabriela.osu@gmail.com>
2014-05-13 17:47:45 -0700

    Refactor and clean code to delete tests that are not being used anymore after refactoring its class

--- test/PlateauTest.java
+++ test/PlateauTest.java
@@ -1,17 +1,4 @@
-import org.junit.Test;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
 public class PlateauTest {
-    @Test
-    public void shouldAddRoverToPlateau(){
-        Rover rover1 = mock(Rover.class);
-        when(rover1.currentPosition()).thenReturn("1 3 N");
 
-//        Plateau plateau = new Plateau();
-//        plateau.add(rover1);
-//
-//        assertEquals(plateau.roverInPlateau(), "1 3 N");
-    }
+
 }
--- test/RoverParserTest.java
+++ test/RoverParserTest.java
@@ -1,32 +1,39 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
 
 public class RoverParserTest {
+    private String fileName;
+    private RoverParser roverparser;
 
+    @Before
+    public void setUp(){
+       fileName = "resources/sampleInput.txt";
+       roverparser = new RoverParser(fileName);
+       roverparser.parseFile();
 
-    @Test
-    public void shouldExtractXCoordinateFromFileAsInteger(){
-        String fileName = "resources/sampleInput.txt";
-
-        int xCoordinate = new RoverParser(fileName).getXCoordinate();
-        assertEquals(xCoordinate, 1);
     }
 
     @Test
-    public void shouldExtractYCoordinateFromFileAsInteger(){
-        String fileName = "resources/sampleInput.txt";
-
-        int yCoordinates = new RoverParser(fileName).getYCoordinate();
-        assertEquals(yCoordinates, 3);
+    public void shouldCreatePlateauObject(){
+        Plateau plateau = new Plateau(5, 5);
+        assertEquals(plateau,roverparser.createPlateau("5 5"));
     }
+
+
 
     @Test
-    public void shouldExtractDirectionFromFileAsString(){
-        String fileName = "resources/sampleInput.txt";
+    public void shouldCreateARoverObject(){
+        ArrayList<String> direction = new ArrayList<String>(Arrays.asList("L", "M", "L"));
+        Rover rover = new Rover(1, 2, "N", direction);
 
-        String direction = new RoverParser(fileName).getDirection();
-        assertEquals(direction, "N");
+        assertEquals(rover, roverparser.createRover("1 2 N", "LML"));
     }
+
+
 
 }
