2410a457e70aacdca943ce78e4b3495ed9fb9fd5
evagabriela <gabriela.osu@gmail.com>
2014-05-17 18:47:14 -0700

    add extra tests for Rover test to prevent the prior bug

--- test/RoverTest.java
+++ test/RoverTest.java
@@ -30,9 +30,81 @@ public class RoverTest {
     }
 
     @Test
+    public void shouldMoveLeftFourTimes(){
+        rover.turnLeft();
+        rover.turnLeft();
+        rover.turnLeft();
+        rover.turnLeft();
+        assertEquals("1 3 N",rover.currentPosition());
+    }
+
+    @Test
+    public void shouldFaceNorthIfTurnLeft(){
+        rover = new Rover(1, 3, "E", "LMLM");
+        rover.turnLeft();
+        assertEquals("1 3 N",rover.currentPosition());
+    }
+
+    @Test
+    public void shouldFaceWestIfTurnLeft(){
+        rover.turnLeft();
+        assertEquals("1 3 W",rover.currentPosition());
+    }
+
+    @Test
+    public void shouldFaceSouthIfTurnLeft(){
+        rover = new Rover(1, 3, "W", "LMLM");
+        rover.turnLeft();
+        assertEquals("1 3 S",rover.currentPosition());
+    }
+
+    @Test
+    public void shouldFaceEastIfTurnLeft(){
+        rover = new Rover(1, 3, "S", "LMLM");
+        rover.turnLeft();
+        assertEquals("1 3 E",rover.currentPosition());
+    }
+
+    @Test
     public void shouldMoveRight(){
         rover.turnRight();
         assertEquals("1 3 E",rover.currentPosition());
+    }
+
+    @Test
+    public void shouldFaceNorthIfTurnRight(){
+        rover = new Rover(1, 3, "E", "LMLM");
+        rover.turnRight();
+        assertEquals("1 3 S",rover.currentPosition());
+    }
+
+    @Test
+    public void shouldFaceWestIfTurnRight(){
+        rover.turnRight();
+        assertEquals("1 3 E",rover.currentPosition());
+    }
+
+    @Test
+    public void shouldFaceSouthIfTurnRight(){
+        rover = new Rover(1, 3, "W", "LMLM");
+        rover.turnRight();
+        assertEquals("1 3 N",rover.currentPosition());
+    }
+
+    @Test
+    public void shouldFaceEastIfTurnRight(){
+        rover = new Rover(1, 3, "S", "LMLM");
+        rover.turnRight();
+        assertEquals("1 3 W",rover.currentPosition());
+    }
+
+    @Test
+    public void shouldMoveRightFourTimes(){
+        rover.turnRight();
+        rover.turnRight();
+        rover.turnRight();
+        rover.turnRight();
+        assertEquals("1 3 N",rover.currentPosition());
     }
 
     @Test
