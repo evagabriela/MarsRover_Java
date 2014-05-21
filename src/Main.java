886acdaedd01b878d0066e862c13585df2db523c
evagabriela <gabriela.osu@gmail.com>
2014-05-21 10:55:51 -0700

    Add PrintStream and clean main code

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,16 @@
 import java.io.IOException;
+import java.io.PrintStream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        PrintStream out = System.out;
         String fileName = "resources/sampleInput.txt";
-        RoverParser roverParser = new RoverParser(fileName);
+        RoverParser roverParser = new RoverParser(fileName, out);
         Plateau plateau =  roverParser.plateauSetUp();
 
         plateau.runRoverInstructions();
 
-        System.out.println( plateau.showRoverFinalPositions());
+        plateau.showRoverFinalPositions();
     }
 }
