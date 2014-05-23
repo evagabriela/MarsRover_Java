4c2f48e1b76e21ef24c32aec908fc48bddcfb81d
evagabriela <gabriela.osu@gmail.com>
2014-05-23 09:57:48 -0700

    Add MoveCommand which calls the move() method when command is call

--- src/MoveCommand.java
+++ src/MoveCommand.java
@@ -1,5 +1,16 @@
 /**
  * Created by gzamudio on 5/23/14.
  */
-public class MoveCommand {
+public class MoveCommand implements Command {
+
+    private Rover rover;
+
+    public MoveCommand(Rover rover){
+        this.rover = rover;
+    }
+
+    @Override
+    public void execute(){
+        rover.move();
+    }
 }
