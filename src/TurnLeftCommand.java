39e91f4c567f241e20ea8388fe08b445690007cf
evagabriela <gabriela.osu@gmail.com>
2014-05-23 09:59:12 -0700

    Add classes that call execute method which call its respective instruction

--- src/TurnLeftCommand.java
+++ src/TurnLeftCommand.java
@@ -1,5 +1,16 @@
 /**
  * Created by gzamudio on 5/23/14.
  */
-public class TurnLeftCommand {
+public class TurnLeftCommand implements Command {
+
+    private Rover rover;
+
+    public TurnLeftCommand(Rover rover){
+        this.rover = rover;
+    }
+
+    @Override
+    public void execute(){
+        rover.turnLeft();
+    }
 }
--- src/TurnRightCommand.java
+++ src/TurnRightCommand.java
@@ -1,5 +1,16 @@
 /**
  * Created by gzamudio on 5/23/14.
  */
-public class TurnRightCommand {
+public class TurnRightCommand implements Command {
+
+    private Rover rover;
+
+    public TurnRightCommand(Rover rover){
+        this.rover = rover;
+    }
+
+    @Override
+    public void execute(){
+        rover.turnRight();
+    }
 }
